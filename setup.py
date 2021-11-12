51d5fc54bf60205651f69c1ea4bd33ea2969ffb3
Jana Kiederle <jana.kiederle@fau.de>
2021-11-12 16:10:22 +0100

    getting right structure

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from distutils.core import setup
+from setuptools import find_packages
+setup(name='fe09fatu',
+version='0.1',
+author='DSSS',
+author_email='jana.kiederle@fau.de',
+packages=find_packages(),
+install_requires=['numpy', 'Pillow', 'ipywidgets'])
+
