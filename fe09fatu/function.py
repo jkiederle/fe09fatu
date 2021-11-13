5bebbe185b90cf7592172fcf99852caf03f25f66
Jana Kiederle <jana.kiederle@fau.de>
2021-11-13 22:20:48 +0100

    try using py instead of notebook

--- fe09fatu/function.py
+++ fe09fatu/function.py
@@ -2,19 +2,6 @@ import numpy as np
 from ipywidgets import interact, fixed
 from PIL import Image
 def imshow(X, resize=None):
-    """
-    You should create a way to resize an image from an array X.
-    The use of widgets is optional but you can take a look to interact.
-    We should be able to install this package in Google Colab from your Git
-    repo.
-    I assume resize is the image and X is the array telling us the size as the instructions are not very clear.
-    """
     if resize:
         X = np.resize(X, resize)
     return X
-
-
-
-
-
-
--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 from distutils.core import setup
 from setuptools import find_packages
 setup(name='fe09fatu',
